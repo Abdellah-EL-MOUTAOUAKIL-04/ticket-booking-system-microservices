58259f13cf00e273f0652100d6258e79cc7988f9
Abdellah EL MOUTAOUKIL <abdamota123@gmail.com>
2025-08-23 23:48:08 +0100

    init commit

---
+++ inventory-service/src/test/java/ma/abdellah_el_moutaouakil/inventoryservice/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.abdellah_el_moutaouakil.inventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
