ba722cf857ff357cbc565eb0c52fe19c4cca961f
Abdellah EL MOUTAOUKIL <abdamota123@gmail.com>
2025-08-30 00:41:35 +0100

    feat:added discovery and gateway service

---
+++ discovery-service/src/test/java/ma/abdellah_el_moutaouakil/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.abdellah_el_moutaouakil.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
